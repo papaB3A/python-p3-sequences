ee4a25cfde37c62ed82332bede6fd08b02f05133
papaB3A <papa.akumu@student.moringaschool.com>
2024-11-26 08:23:26 +0300

    Completed python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
+def fibonacci(number):
+    # base/stopping case
+    if number<=1:
+        return number
+    return fibonacci(number- 1) +  fibonacci(number- 2)
     pass
+
+# def print_fibonacci(length):
+#     ar_ray=[]
+#     for number in range(0, length+ 1):
+#         # print(fibonacci(number))
+#         # ar_ray.append(fibonacci(number))
+#         if (not number )== 0:
+#             ar_ray.append(fibonacci(number))
+#     print(ar_ray)
+#     pass
+
+def print_fibonacci(length):
+    # Generate the Fibonacci sequence as a list
+    fib_sequence = [fibonacci(i) for i in range(length)]
+    print(fib_sequence)
+    pass
+
+print_fibonacci(9)
